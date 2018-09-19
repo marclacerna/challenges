106ba07f1accfaa928c5372b1c7a9ab5b694c2c9
marclacerna <marcyvan.lacerna@gmail.com>
2018-09-19 12:58:55 -0400

    make code more pythonic

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -4,25 +4,22 @@ from data import DICTIONARY, LETTER_SCORES
 def load_words():
     """Load dictionary into a list and return list"""
     with open(DICTIONARY) as f:
-        dic_val = [line.strip('\n') for line in f]
-    return dic_val
+        return [line.strip('\n') for line in f]
 
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    score = [LETTER_SCORES[i.upper()] for i in word if i.isalpha()]
-
-    return sum(score)
+    return sum([LETTER_SCORES[i.upper()] for i in word if i.isalpha()])
 
 
-def max_word_value(*args):
+def max_word_value(args=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    dic_val = load_words() if len(args) == 0 else [i for i in args[0]]
-    score_name_dic = {calc_word_value(i): i for i in dic_val}
+    if args is None:
+        args = load_words()
 
-    return score_name_dic[max(score_name_dic)]
+    return max(args, key=calc_word_value)
 
 
 if __name__ == "__main__":
